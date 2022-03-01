bbeda029341c0b4b93ed7a076f701f8984e6a066
agif <alpenizme@gmail.com>
2022-03-01 22:15:53 +0700

    Deploying to Heroku

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      localhost:3000
+// @host      localhost:8080
 // @BasePath  /api/v1
 
 // @securityDefinitions.apikey  ApiKeyAuth
